226ef949c8c37d57504e069c5217a34d60199419
James Terry <jamesnorthterry@gmail.com>
2017-10-16 14:36:40 -0700

    ES6d those bees

--- src/RetiredForagerBee.js
+++ src/RetiredForagerBee.js
@@ -1,3 +1,37 @@
-class RetiredForagerBee {
-  // TODO..
+class RetiredForagerBee extends ForagerBee {
+  constructor() {
+    super();
+    this.age = 40;
+    this.job = 'gamble';
+    this.canFly = false;
+    this.color = 'grey';
+  }
+
+  forage(treasure) {
+    return 'I am too old, let me play cards instead';
+  }
+
+  gamble(treasure) {
+    this.treasureChest.push(treasure)
+  }
 };
+
+// 
+// var RetiredForagerBee = function() {
+//   ForagerBee.call(this);
+//   this.age = 40;
+//   this.job = 'gamble';
+//   this.canFly = false;
+//   this.color = 'grey';
+// };
+//
+// RetiredForagerBee.prototype = Object.create(ForagerBee.prototype);
+// RetiredForagerBee.prototype.constructor = RetiredForagerBee;
+//
+// RetiredForagerBee.prototype.forage = function(treasure) {
+//   return 'I am too old, let me play cards instead';
+// };
+//
+// RetiredForagerBee.prototype.gamble = function(treasure) {
+//   this.treasureChest.push(treasure)
+// };
